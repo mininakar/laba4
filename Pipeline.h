d7ae0fb872e17c01eb6740beacef2e004d95699d
mininakar <minyailo.karina@gmail.com>
2022-05-20 22:19:35 +0500

    23

---
+++ Pipeline.h
@@ -0,0 +1,82 @@
+#pragma once
+#define	PIPELINE_H
+#include <GL/glew.h> // extensions manager
+#include <GL/freeglut.h> //GLUT - OpenGL Utility Library - API for managing the window system, as well as event handling, input/output control
+#include <glm/glm.hpp>	//#include "math_3d.h" - vector
+//#include "math_3d.h"
+
+class Pipeline
+{
+public:
+    Pipeline()
+    {
+        m_scale = glm::vec3 (1.0f, 1.0f, 1.0f);
+        m_worldPos = glm::vec3(0.0f, 0.0f, 0.0f);
+        m_rotateInfo = glm::vec3 (0.0f, 0.0f, 0.0f);
+    }
+
+    void Scale(float ScaleX, float ScaleY, float ScaleZ)
+    {
+        m_scale.x = ScaleX;
+        m_scale.y = ScaleY;
+        m_scale.z = ScaleZ;
+    }
+
+    void WorldPos(float x, float y, float z)
+    {
+        m_worldPos.x = x;
+        m_worldPos.y = y;
+        m_worldPos.z = z;
+    }
+
+    void Rotate(float RotateX, float RotateY, float RotateZ)
+    {
+        m_rotateInfo.x = RotateX;
+        m_rotateInfo.y = RotateY;
+        m_rotateInfo.z = RotateZ;
+    }
+
+    void SetPerspectiveProj(float FOV, float Width, float Height, float zNear, float zFar)
+    {
+        m_persProj.FOV = FOV;
+        m_persProj.Width = Width;
+        m_persProj.Height = Height;
+        m_persProj.zNear = zNear;
+        m_persProj.zFar = zFar;
+    }
+
+    void SetCamera(const glm::vec3& Pos, const glm::vec3& Target, const glm::vec3& Up)
+    {
+        m_camera.Pos = Pos;
+        m_camera.Target = Target;
+        m_camera.Up = Up;
+    }
+
+
+    const glm::mat4& GetWVPTrans();
+
+    const glm::mat4& GetWorldTrans();
+
+
+private:
+    glm::vec3 m_scale;
+    glm::vec3 m_worldPos;
+    glm::vec3 m_rotateInfo;
+
+    struct {
+        float FOV;
+        float Width;
+        float Height;
+        float zNear;
+        float zFar;
+    } m_persProj;
+
+    struct {
+        glm::vec3 Pos;
+        glm::vec3 Target;
+        glm::vec3 Up;
+    } m_camera;
+
+    glm::mat4& m_WVPtransformation;
+    glm::mat4& m_WorldTransformation;
+};
---
+++ mesh.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <GL/glew.h> // extensions manager
+#include <GL/freeglut.h> //GLUT - OpenGL Utility Library - API for managing the window system, as well as event handling, input/output control
+#include <glm/glm.hpp>	//#include "math_3d.h" - vector
+#include <string> 
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+struct Texture {
+    unsigned int id;
+    string type;
+};
+class Mesh
+{
+    public:
+        // Mesh-данные
+        vector<Vertex>       vertices;
+        vector<unsigned int> indices;
+        vector<Texture>      textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+        void Draw(Shader shader);
+    private:
+        // Данные для рендеринга
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
+    };
+};
+
